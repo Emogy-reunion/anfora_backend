dbc5dbc1b18796b5f93bfa55492bfb8c4dceeac8
Emogy-reunion <riot@DESKTOP-CFMPLA3.localdomain>
2026-07-06 15:25:31 -0700

    add: add a method that initializes the table with data and hashes the password

--- core/models.py
+++ core/models.py
@@ -6,8 +6,24 @@ class User(db.Model):
     '''
     stores the user data
     '''
+    __tablename__ = 'users'
+
     id = db.Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, index=True, unique=True)
     password_hash = db.Column(db.String(255), nullable=False)
     logo = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, email, password, logo):
+        '''
+        instantializes the table with data
+        '''
+        self.email = email
+        self.password = User.hash_password(password)
+
+    @staticmethod
+    def hash_password(self, password):
+        '''
+        converts the password to a hash for security purposes
+        '''
+        return bcrypt.generate_password_hash(password).decode('utf-8')
